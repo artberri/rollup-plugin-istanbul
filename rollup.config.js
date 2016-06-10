14246668570852c1c2f438d4259959c4dea68230
Brian Donovan <donovan@squareup.com>
2016-06-10 07:07:15 -0700

    Use rollup targets to speed up the build.
    This also pulls in other changes between 0.25.1 and 0.30.0, notably support for the __esModule flag.

--- rollup.config.js
+++ rollup.config.js
@@ -3,5 +3,15 @@ import babel from 'rollup-plugin-babel'
 export default {
   entry: 'src/index.js',
   plugins: [babel()],
-  external: ['istanbul', 'rollup-pluginutils']
+  external: ['istanbul', 'rollup-pluginutils'],
+  targets: [
+    {
+      format: 'cjs',
+      dest: 'dist/rollup-plugin-istanbul.cjs.js'
+    },
+    {
+      format: 'es6',
+      dest: 'dist/rollup-plugin-istanbul.es6.js'
+    }
+  ]
 }
